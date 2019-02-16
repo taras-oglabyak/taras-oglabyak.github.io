a4174efd178f9e50fd830cd317e12062c4c66aba
Taras Oglabyak <tohl@softserveinc.com>
2019-02-16 15:07:05 +0200

    x

---
+++ precache-manifest.f060d3d877effafa655c7b7687ffd33d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c86b25462b37d08eacd7",
+    "url": "/static/css/main.9f1bab67.chunk.css"
+  },
+  {
+    "revision": "c86b25462b37d08eacd7",
+    "url": "/static/js/main.c86b2546.chunk.js"
+  },
+  {
+    "revision": "784b691c36a161194eb4",
+    "url": "/static/js/1.784b691c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1e59b4f3cf1d3f40fa7f58db7bbb1a5",
+    "url": "/index.html"
+  }
+];
